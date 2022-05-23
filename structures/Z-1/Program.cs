d94fd4238952df87813cfcdbd8b9a1caab0147b1
tomelovich <tomelovichs@gmail.com>
2022-05-23 19:30:41 +0300

    Выполнено

---
+++ structures/Z-1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace Z_1
+{
+    class Program
+    {
+        public struct TRAIN
+        {
+            public string destination;
+            public int number;
+            public int[] time;   
+        }
+        static void Main(string[] args)
+        {
+            TRAIN[] massTrain = new TRAIN[2];
+            for (int i = 0; i < massTrain.Length; i++)
+            {
+                Console.WriteLine("Введите пункт назначения: ");
+                massTrain[i].destination = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите номер поезда: ");
+                massTrain[i].number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите время: ");
+                massTrain[i].time = new int[2];
+                for (int j = 0; j < massTrain[i].time.Length; j++)
+                    massTrain[i].time[j] = int.Parse(Console.ReadLine());
+            }            
+            for (int i = 0; i < massTrain.Length; i++)
+            {
+                MyClass.SortAlph(massTrain);
+                Console.WriteLine("TRAIN" + " " + (i + 1) + ":");
+                Console.WriteLine("Пункт назначения: " + massTrain[i].destination);
+                Console.WriteLine("Номер поезда: " + massTrain[i].number);
+                Console.Write("Время");
+                for (int j = 0; j < massTrain[i].time.Length; j++)
+                    Console.Write(": " + massTrain[i].time[j]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Введите время для поиска:");
+            int[] timeSearch = new int[2];
+            for (int j = 0; j < timeSearch.Length; j++)
+                timeSearch[j] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < massTrain.Length; i++)
+            {
+                if ((massTrain[i].time[0] > timeSearch[0])) 
+                {
+                    Console.WriteLine("Пункт назначения: " + massTrain[i].destination);
+                    Console.WriteLine("Номер поезда: " + massTrain[i].number);
+                    Console.Write("Время");
+                    for (int j = 0; j < massTrain[i].time.Length; j++)
+                        Console.Write(": " + massTrain[i].time[j]);
+                    Console.WriteLine();
+                }
+                else if ((massTrain[i].time[0] == timeSearch[0]))
+                {
+                    if ((massTrain[i].time[1] > timeSearch[1]))
+                    {
+                        Console.WriteLine("Пункт назначения: " + massTrain[i].destination);
+                        Console.WriteLine("Номер поезда: " + massTrain[i].number);
+                        Console.WriteLine("Время:");
+                        for (int j = 0; j < massTrain[i].time.Length; j++)
+                            Console.Write(": " + massTrain[i].time[j]);
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Не найдено");
+                }
+            }
+            Console.ReadKey();
+        }
+        public static class MyClass
+        {
+            public static void SortAlph(TRAIN[] trains)
+            {
+                var query = from u in trains
+                            orderby u.destination
+                            select u;
+                int i = 0;
+                foreach (var item in query)
+                {
+                    trains[i] = item;
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ structures/Z-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Z_2
+{
+    class Program
+    {
+        class Accauntant
+        {
+            public enum Employees
+            {
+                ChiefExecutiveOfficer = 220,
+                ChiefTechnologyOfficer = 250,
+                JuniorDeveloper = 210,
+                MiddleDeveloper = 240,
+                SeniorDeveloper = 220,
+                QualityAssuranceEngineers = 270
+            }
+            public void AskForBonus(Employees postWorker, int hours)
+            {                              
+                if ((int)postWorker < hours)
+                {
+                    Console.WriteLine("Необходимо начислить премию");
+                }
+                else
+                {
+                    Console.WriteLine("Начислять премию не нужно");
+                }
+            }    
+        }
+        static void Main(string[] args)
+        {
+            Accauntant с = new Accauntant();
+            с.AskForBonus(Accauntant.Employees.ChiefExecutiveOfficer, 240);
+            с.AskForBonus(Accauntant.Employees.ChiefTechnologyOfficer, 220);
+            с.AskForBonus(Accauntant.Employees.JuniorDeveloper, 235);
+            с.AskForBonus(Accauntant.Employees.MiddleDeveloper, 225);
+            с.AskForBonus(Accauntant.Employees.SeniorDeveloper, 240);
+            с.AskForBonus(Accauntant.Employees.QualityAssuranceEngineers, 215);
+        }
+    }
+}
